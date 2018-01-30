c2990c30037cce377eaa504030d7570ab420e17f
Unknown <colin.hite@gmail.com>
2018-01-29 23:49:10 -0700

    made progress on the shooting mech

---
+++ DGM-2610-GameDesign/FileStruct/Assets/Scripts/Character/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public GameObject projectile;
+    public Transform projSpawn;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        //projectile = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            FireProjectile();
+        }
+        projectile.AddForce(transform.forward * speed);
+	}
+
+    void FireProjectile()
+    {
+
+    }
+}
