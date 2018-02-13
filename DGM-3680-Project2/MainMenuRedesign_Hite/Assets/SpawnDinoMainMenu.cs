9ac5a4dc576cbbe0862948b724e6bafe16dcf9f9
Unknown <colin.hite@gmail.com>
2018-02-12 18:58:35 -0700

    Add junior project

---
+++ DGM-3680-Project2/MainMenuRedesign_Hite/Assets/SpawnDinoMainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDinoMainMenu : MonoBehaviour {
+
+	public GameObject dinoSpawn;
+
+	public GameObject[] dinos;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//dinos["random number"].transform.position = dinoSpawn.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public void SpawnDino (int i)
+	{
+		dinos[i].transform.position = dinoSpawn.transform.position;
+		dinos[i].transform.rotation = dinoSpawn.transform.rotation;
+	}
+
+}
---
+++ DGM-3680-Project2/UITeam/MenuNavMock_Hite/screen_nav_mock/Assets/ChangeMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMenu : MonoBehaviour {
+
+    public GameObject toClose;
+    public GameObject toOpen;
+
+    public void ChangeVisable ()
+    {
+        toClose.SetActive(false);
+        toOpen.SetActive(true);
+        Debug.Log("it got pressed");
+    }
+}
