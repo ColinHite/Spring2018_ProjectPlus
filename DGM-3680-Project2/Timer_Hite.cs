0657c028f9ed4e223cffcf9f563a4a1516813075
Unknown <colin.hite@gmail.com>
2018-02-28 12:52:33 -0700

    Got the timer script done

---
+++ DGM-3680-Project2/Timer_Hite.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer_Hite : MonoBehaviour {
+
+	public float timer_length;
+
+	public GameObject[] dino_sel;
+
+	public Transform spawn_loc;
+
+
+	public Transform player_menu;
+
+	public GameObject player;
+
+	public void CallTimer()
+	{
+		dino_sel[Random.Range(0, 3)].transform.position = spawn_loc.transform.position;
+
+		//return the player to the menu location
+		player.transform.position = player_menu.transform.position;
+	}
+
+}
