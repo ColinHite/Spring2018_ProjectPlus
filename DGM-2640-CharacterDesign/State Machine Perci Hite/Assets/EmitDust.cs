1704c7365a7614add2759d48998a13a32a375131
Unknown <colin.hite@gmail.com>
2018-03-06 14:14:28 -0700

    Got the animation state machine event thing working

---
+++ DGM-2640-CharacterDesign/State Machine Perci Hite/Assets/EmitDust.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmitDust : MonoBehaviour {
+
+	public ParticleSystem dustParticles;
+
+	public void Start()
+	{
+		//dustParticles.emission.
+		// ();
+	}
+
+	public IEnumerator Wait()
+	{
+		yield return new WaitForSecondsRealtime (0.1f);
+		//dustParticles.SetActive (false);
+	}
+
+}
