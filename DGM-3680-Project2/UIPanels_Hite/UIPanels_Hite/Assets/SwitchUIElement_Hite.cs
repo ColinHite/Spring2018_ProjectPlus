d257d7427068a0dc6472b2236fe7db4fc3e8a193
Unknown <colin.hite@gmail.com>
2018-03-11 23:37:58 -0600

    Started the panels for the project

---
+++ DGM-3680-Project2/UIPanels_Hite/UIPanels_Hite/Assets/SwitchUIElement_Hite.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchUIElement_Hite : MonoBehaviour {
+
+    public GameObject[] ui_disable;
+    public GameObject[] ui_enable;
+
+public void SwitchUI()
+    {
+        for (int i = 0; i < ui_disable.Length; i++)
+        {
+            ui_disable[i].SetActive(false);
+            print("Item" + i + "was disabled.");
+        }
+
+        for (int j = 0; j < ui_enable.Length; j++)
+        {
+            ui_enable[j].SetActive(true);
+            print("Item" + j + "was enabled.");
+        }
+        print("The Call Worked");
+    }
+}
