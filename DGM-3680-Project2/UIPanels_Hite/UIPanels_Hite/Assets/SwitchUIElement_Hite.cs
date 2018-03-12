ae8119ef2676914a47f87e138da49102265548cc
Unknown <colin.hite@gmail.com>
2018-03-12 10:30:15 -0600

    Am fixing stuff

--- DGM-3680-Project2/UIPanels_Hite/UIPanels_Hite/Assets/SwitchUIElement_Hite.cs
+++ DGM-3680-Project2/UIPanels_Hite/UIPanels_Hite/Assets/SwitchUIElement_Hite.cs
@@ -4,22 +4,14 @@ using UnityEngine;
 
 public class SwitchUIElement_Hite : MonoBehaviour {
 
-    public GameObject[] ui_disable;
-    public GameObject[] ui_enable;
+    public GameObject ui_disable;
+    public GameObject ui_enable;
 
 public void SwitchUI()
     {
-        for (int i = 0; i < ui_disable.Length; i++)
-        {
-            ui_disable[i].SetActive(false);
-            print("Item" + i + "was disabled.");
-        }
 
-        for (int j = 0; j < ui_enable.Length; j++)
-        {
-            ui_enable[j].SetActive(true);
-            print("Item" + j + "was enabled.");
-        }
-        print("The Call Worked");
+            ui_disable.SetActive(false);
+            ui_enable.SetActive(true);
+
     }
 }
