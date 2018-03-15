65efe215e260b9a3a1c96614b6e51112a109f9d8
Unknown <colin.hite@gmail.com>
2018-03-14 19:41:33 -0600

    Got some stuff done with the UI

---
+++ DGM-3680-Project2/UIPanels_Hite/UIPanels_Hite/Assets/QuitGame_Hite.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame_Hite : MonoBehaviour {
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        print("you quit the game.");
+    }
+
+}
