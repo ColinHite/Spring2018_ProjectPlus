984d35e210d430f4f6f99dfe27a75a5886a580e2
Unknown <colin.hite@gmail.com>
2018-04-11 12:18:21 -0600

    Got the results screen to work

---
+++ DGM-3680-Project2/resultsScreen_UI_Hite/Assets/ChangeRoundText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeRoundText : MonoBehaviour {
+
+	public Text roundText;
+
+	public void ChangeText (int winLoss)
+	{
+		if (winLoss == 1) {
+			roundText.text = roundText.text + "Victory";
+		}
+		if (winLoss == 0) {
+			roundText.text = roundText.text + "Failure";
+		} else {
+			roundText.text = roundText.text + "Tie";
+		}
+	}
+}
