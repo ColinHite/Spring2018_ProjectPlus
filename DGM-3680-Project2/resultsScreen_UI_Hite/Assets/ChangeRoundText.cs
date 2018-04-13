ef4f33ae9d81fd2c4e5b792e4e56caa1d4602b5e
Unknown <colin.hite@gmail.com>
2018-04-13 10:19:03 -0600

    Some stuff got changed?

--- DGM-3680-Project2/resultsScreen_UI_Hite/Assets/ChangeRoundText.cs
+++ DGM-3680-Project2/resultsScreen_UI_Hite/Assets/ChangeRoundText.cs
@@ -1,4 +1,12 @@
-﻿using System.Collections;
+﻿/// <summary>
+/// This script takes an int value which changes the text of the assigned object. 0 = loss text, 1 = victory text, 2 = tie
+/// 
+/// Assign this script to something and assign the text object to its field and then call the "ChangeText" method.
+/// 
+/// Author: Colin Hite
+/// </summary>
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,7 +22,8 @@ public class ChangeRoundText : MonoBehaviour {
 		}
 		if (winLoss == 0) {
 			roundText.text = roundText.text + "Failure";
-		} else {
+		} 
+		if (winLoss == 2){
 			roundText.text = roundText.text + "Tie";
 		}
 	}
