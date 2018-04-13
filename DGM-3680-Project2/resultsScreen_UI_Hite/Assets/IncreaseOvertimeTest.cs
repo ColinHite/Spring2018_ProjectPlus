71b74fbcbb6b9062bd18d62ec5c47e050707c800
Unknown <colin.hite@gmail.com>
2018-04-13 12:53:02 -0600

    Am starting the stepperScript

---
+++ DGM-3680-Project2/resultsScreen_UI_Hite/Assets/IncreaseOvertimeTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncreaseOvertimeTest : MonoBehaviour {
+
+	public float stepFloat;
+	public float multiplier;
+
+	public float rangeMax = 100;
+	public float rangeMin = 0;
+
+	void Start () {
+		
+		StartCoroutine (WaitYouSillyBean());
+
+	}
+
+	public IEnumerator WaitYouSillyBean()
+	{
+		while (rangeMin <= rangeMax) {
+			
+			yield return new WaitForSecondsRealtime (0.5f);
+
+			stepFloat++;
+
+			rangeMin++;
+
+			print (stepFloat);
+
+		}
+	}
+}
